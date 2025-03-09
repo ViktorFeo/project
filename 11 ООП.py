a9cbcf0f7bd270634b5302e8f50fd6ee1fb3815e
ViktorFeo <vsf.viktor@gmail.com>
2025-03-09 17:11:34 +0300

    oop

--- 11 ООП.py
+++ 11 ООП.py
@@ -62,7 +62,7 @@ print(user_1.user_name, user_2.user_name)  # Вывод: Noname Noname
 
 class Toyota:
 
-    def __init__(self, color, price, max_speed, current_speed):
+    def __init__(self, color='red', price=1e6, max_speed=200, current_speed=0):
         self.color = color
         self.price = price
         self.max_speed = max_speed
@@ -192,5 +192,64 @@ emp_1.print_info()  # Вывод информации о первом сотру
 emp_2.print_info()  # Вывод информации о втором сотруднике (emp_2)
 
 
-class potato:
-    pass
+class Potato:
+    states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Зрелая'}
+
+    def __init__(self, index):
+        self.index = index
+        self.state = 0
+
+    def grow(self):
+        if self.state < 3:
+            self.state += 1
+        self.print_state()
+
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
+
+    def print_state(self):
+        print('Картошка {} сейчас {}'.format(self.index, Potato.states[self.state]))
+
+
+class PotatoGarden:
+
+    def __init__(self, count):
+        self.potatoes = [Potato(index) for index in range(1, count + 1)]
+
+    def grow_all(self):
+        print('Картошка прорастает!')
+        for i_potato in self.potatoes:
+            i_potato.grow()
+
+    def are_all_ripe(self):
+        if not all([i_potato.is_ripe() for i_potato in self.potatoes]):
+            print('Картошка еще не созрела!\n')
+        else:
+            print('Вся картошка созрела. Можно собирать')
+
+
+my_garden = PotatoGarden(5)
+my_garden.are_all_ripe()
+for _ in range(3):
+    my_garden.grow_all()
+    my_garden.are_all_ripe()
+
+class Point:
+    count = 0
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        Point.count += 1
+
+    def info(self):
+        return 'Точка с координатами {}, {}'.format(self.x, self.y)
+
+
+p1 = Point()
+print(p1.info())
+p2 = Point(3, 4)
+print(p2.info())
+print(Point.count)
